3a7269a25e92c3788a8bc17f455b4352d3719301
Orlson Aine Joseph <ojoseph@princeton.edu>
2022-08-28 05:39:06 -0400

    f

--- core/static/core/js/connect.js
+++ core/static/core/js/connect.js
@@ -1,19 +1,19 @@
 document.getElementById('jQueryLoad').addEventListener('load', function () {
-    console.log("ready");
-    const authentication_token = $("#auth_token").val();
-    const editorExtensionId = "mchcicmnbpcopfmklilbmbjgdmhlkapf";
+  console.log("ready");
+  const authentication_token = $("#auth_token").val();
+  const editorExtensionId = "bjemlddlpahnmmjidgpobkbpkdackejp";
 
-    $("#connectForm").submit(function (event) {
-        event.preventDefault();
+  $("#connectForm").submit(function (event) {
+    event.preventDefault();
 
-        console.log("Making request");
+    console.log("Making request");
 
-        // Make a simple request:
-        chrome.runtime.sendMessage(editorExtensionId, { token: authentication_token },
-            function (response) {
-                console.log(response);
-                if (!response.success)
-                    handleError(authentication_token);
-            });
-    });
+    // Make a simple request:
+    chrome.runtime.sendMessage(editorExtensionId, { token: authentication_token },
+      function (response) {
+        console.log(response);
+        if (!response.success)
+          handleError(authentication_token);
+      });
+  });
 });
